f59ac7a073c413dd7025d8d4d6845c0629f3e5cf
Tomas Kriska <kriskatomas360@gmail.com>
2020-03-05 23:07:15 +0100

    added new project for migrations

---
+++ Badzeet.Book.Migrator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Badzeet.Book.Migrator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var connectionString = "";
+        }
+    }
+}
---
+++ Badzeet.Book.Migrator/Scripts/0001_Create_Transactions.sql
@@ -0,0 +1,7 @@
+﻿create table [book].[transactions](
+	[id] [bigint] identity(1,1) primary key,
+	[account_id] [bigint] not null,
+	[date] [date] not null,
+	[amount] [decimal](10, 2) not null,
+	[description] [nvarchar](100) null
+)
