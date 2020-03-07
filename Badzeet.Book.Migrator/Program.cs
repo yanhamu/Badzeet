65dd319279232c5bf9eb03744962938976050098
Tomas Kriska <kriskatomas360@gmail.com>
2020-03-07 14:18:18 +0100

    added migrator

--- Badzeet.Book.Migrator/Program.cs
+++ Badzeet.Book.Migrator/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DbUp;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Reflection;
 
 namespace Badzeet.Book.Migrator
 {
@@ -6,8 +9,35 @@ namespace Badzeet.Book.Migrator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            var connectionString = "";
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.Development.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("badzeetDb");
+
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader = DeployChanges.To
+                .SqlDatabase(connectionString)
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                .JournalToSqlTable("dbo", "schema_versions")
+                .LogToConsole()
+                .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+                Console.ReadLine();
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
         }
     }
 }
