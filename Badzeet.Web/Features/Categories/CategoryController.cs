27952d8d56d0bc030c793f52ed32f3757e631a02
Tomas Kriska <kriskatomas360@gmail.com>
2020-03-21 17:31:36 +0100

    updated layout

---
+++ Badzeet.Web/Features/Categories/CategoryController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Badzeet.Web.Features.Categories
+{
+    [Authorize]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
