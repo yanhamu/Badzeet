37d0c9e44c591d73e32a72476c9507ca91aae560
Tomas Kriska <tomas.kriska@nanoenergies.cz>
2021-12-04 01:01:59 +0100

    added navigation

---
+++ Badzeet.Web/Api/NavigationController.cs
@@ -0,0 +1,50 @@
+﻿using Badzeet.Budget.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Badzeet.Web.Api
+{
+    [Route("api")]
+    public class NavigationController : ControllerBase
+    {
+        private readonly IBudgetRepository budgetRepository;
+        private readonly IAccountRepository accountRepository;
+
+        public NavigationController(IBudgetRepository budgetRepository, IAccountRepository accountRepository)
+        {
+            this.budgetRepository = budgetRepository;
+            this.accountRepository = accountRepository;
+        }
+
+        [HttpGet("accounts/{accountId:long}/navigations")]
+        public async Task<NavigationResponse> Get(long accountId)
+        {
+            var now = DateTime.UtcNow.Date;
+            var budget = (await budgetRepository.List(accountId, new Filter() { From = now, To = now })).SingleOrDefault();
+            var account = await accountRepository.GetAccount(accountId);
+            var firstDay = (int)account.FirstDayOfTheBudget;
+
+            var budgetDateBoundary = now.AddDays(-now.Day + 1).AddDays(firstDay - 1);
+            if (budgetDateBoundary >= now)
+                return new NavigationResponse(budgetDateBoundary.AddMonths(-1), budgetDateBoundary.AddDays(-1), budget?.Id);
+
+            return new NavigationResponse(budgetDateBoundary, budgetDateBoundary.AddMonths(1).AddDays(-1), budget?.Id);
+        }
+
+        public class NavigationResponse
+        {
+            public NavigationResponse(DateTime from, DateTime to, long? budgetId = null)
+            {
+                BudgetId = budgetId;
+                From = from;
+                To = to;
+            }
+
+            public long? BudgetId { get; set; }
+            public DateTime From { get; set; }
+            public DateTime To { get; set; }
+        }
+    }
+}
