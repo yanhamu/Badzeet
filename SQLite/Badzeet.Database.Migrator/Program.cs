b48367321543f8ca6d381a68a6237f210938a125
Tomas Kriska <tomas.kriska@nanoenergies.cz>
2022-10-02 16:07:49 +0200

    create sqlite scripts

---
+++ SQLite/Badzeet.Database.Migrator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Badzeet.Database.Migrator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
